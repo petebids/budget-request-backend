78ab77c388c271d950ea85d601749d58c3515df6
Pete <petebidstrup1988@gmail.com>
2020-08-19 21:20:15 +1000

    refactored to goggle formattings

--- src/main/java/com/pete/budgetrequestbackend/service/BudgetRequestService.java
+++ src/main/java/com/pete/budgetrequestbackend/service/BudgetRequestService.java
@@ -4,12 +4,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-
-import com.pete.budgetrequestbackend.model.BudgetRequest;
-import com.pete.budgetrequestbackend.payload.CreateBudgetRequestPayload;
-import com.pete.budgetrequestbackend.repository.BudgetRequestRepository;
-import com.pete.budgetrequestbackend.util.DateUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,55 +13,59 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.pete.budgetrequestbackend.model.BudgetRequest;
+import com.pete.budgetrequestbackend.payload.CreateBudgetRequestPayload;
+import com.pete.budgetrequestbackend.repository.BudgetRequestRepository;
+import com.pete.budgetrequestbackend.util.DateUtil;
+
 @Service
-public class BudgetRequestService{
+public class BudgetRequestService {
 
-    @Autowired
-    BudgetRequestRepository budgetRequestRepository ;
+  @Autowired BudgetRequestRepository budgetRequestRepository;
 
-    @Autowired
-    DateUtil dateUtil; 
+  @Autowired DateUtil dateUtil;
 
-    @Value("$budgetrequests.alowable-query-params")
-    List<String> allowableQueryParams;
+  @Value("$budgetrequests.alowable-query-params")
+  List<String> allowableQueryParams;
 
+  public ResponseEntity<BudgetRequest> getByID(Long id) {
+    BudgetRequest budgetRequest =
+        budgetRequestRepository
+            .findById(id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    return ResponseEntity.status(HttpStatus.OK).body(budgetRequest);
+  }
 
-    public ResponseEntity<BudgetRequest> getByID(Long id){
-        BudgetRequest budgetRequest = budgetRequestRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND) );
-        return ResponseEntity.status(HttpStatus.OK).body(budgetRequest);
+  public ResponseEntity<List<BudgetRequest>> findAll() {
+    List<BudgetRequest> budgetRequests = new ArrayList<BudgetRequest>();
+    budgetRequestRepository.findAll().forEach(budgetRequests::add);
+    if (budgetRequests.isEmpty()) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+    return ResponseEntity.status(HttpStatus.OK).body(budgetRequests);
+  }
+
+  public ResponseEntity<List<BudgetRequest>> getAllByParams(Map<String, String> requestParams) {
+    for (String param : requestParams.keySet()) {
+      if (!allowableQueryParams.contains(param)) {
+        throw new ResponseStatusException(
+            HttpStatus.BAD_REQUEST, "query param " + param + " not supported ");
+      }
+    }
+    List<BudgetRequest> budgetRequests = new ArrayList<BudgetRequest>();
+
+    Example<BudgetRequest> example = buildExampleFromRequestParams(requestParams);
+    budgetRequestRepository.findAll(example).forEach(budgetRequests::add);
+    if (budgetRequests.isEmpty()) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 
-    public  ResponseEntity<List<BudgetRequest>> findAll(){
-        List<BudgetRequest> budgetRequests = new ArrayList<BudgetRequest>();
-        budgetRequestRepository.findAll().forEach(budgetRequests::add);
-        if (budgetRequests.isEmpty()){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND) ; 
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(budgetRequests);
-    }
+    return ResponseEntity.status(HttpStatus.OK).body(budgetRequests);
+  }
 
+  public ResponseEntity<BudgetRequest> createBudgetRequest(CreateBudgetRequestPayload payload) {
 
-    public ResponseEntity<List<BudgetRequest>> getAllByParams(Map<String, String> requestParams){
-        for (String param : requestParams.keySet()) {
-            if (!allowableQueryParams.contains(param)){
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST , "query param " +  param + " not supported ");
-            }
-        }
-        List<BudgetRequest> budgetRequests = new ArrayList<BudgetRequest>();
-        
-        Example<BudgetRequest> example = buildExampleFromRequestParams(requestParams);
-        budgetRequestRepository.findAll(example).forEach(budgetRequests::add);
-        if (budgetRequests.isEmpty()){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND) ; 
-        }
-
-        return ResponseEntity.status(HttpStatus.OK).body(budgetRequests);    
-    }
-
-
-    public ResponseEntity<BudgetRequest> createBudgetRequest(CreateBudgetRequestPayload payload){        
-
-    //process inputs as they are 
+    // process inputs as they are
     BudgetRequest budgetRequest = new BudgetRequest();
     budgetRequest.setBranchNumber(payload.getBranchNumber());
     budgetRequest.setCostCentre(payload.getCostCentre());
@@ -78,30 +76,27 @@ public class BudgetRequestService{
     budgetRequest.setProjectBudget(payload.getProjectBudget());
     budgetRequest.setRequestor(payload.getRequestor());
     budgetRequest.setState(payload.getState());
-    
-    // these attribures are defined by bus logic , they are generated here rather than expected to be part of they payload
+
+    // these attribures are defined by bus logic , they are generated here rather than expected to
+    // be part of they payload
     LocalDate now = LocalDate.now();
-    budgetRequest.setAllocationDate(dateUtil.getStartOfMonthFromNow(now)); 
-    budgetRequest.setFinYear(dateUtil.getEndOfCurrentFinYearFromNow(now)); 
+    budgetRequest.setAllocationDate(dateUtil.getStartOfMonthFromNow(now));
+    budgetRequest.setFinYear(dateUtil.getEndOfCurrentFinYearFromNow(now));
     budgetRequest.setCreatedDate(now);
 
-
-
     budgetRequestRepository.save(budgetRequest);
-    return  ResponseEntity.status(HttpStatus.CREATED).body(budgetRequest);
+    return ResponseEntity.status(HttpStatus.CREATED).body(budgetRequest);
+  }
+
+  private Example<BudgetRequest> buildExampleFromRequestParams(Map<String, String> requestParams) {
+    BudgetRequest budgetRequest = new BudgetRequest();
+    if (requestParams.containsKey("branchNumber")) {
+      budgetRequest.setBranchNumber(requestParams.get("branchNumber"));
     }
-
-    private Example<BudgetRequest> buildExampleFromRequestParams(Map<String, String> requestParams){
-        BudgetRequest budgetRequest = new BudgetRequest();
-        if (requestParams.containsKey("branchNumber")){
-            budgetRequest.setBranchNumber(requestParams.get("branchNumber"));
-        }
-        if (requestParams.containsKey("costCentre")){
-            budgetRequest.setCostCentre(requestParams.get("costCentre"));
-        }
-        Example<BudgetRequest>  example = Example.of(budgetRequest);
-        return example;
+    if (requestParams.containsKey("costCentre")) {
+      budgetRequest.setCostCentre(requestParams.get("costCentre"));
     }
-
-
+    Example<BudgetRequest> example = Example.of(budgetRequest);
+    return example;
+  }
 }
